19d847e269b3bb4ed1100e63d7ffeae95540aaa2
Terris Kremer <terris.kremer@gmail.com>
2020-02-26 16:17:44 -0500

    fixup add scores

--- src/components/games/hooks.js
+++ src/components/games/hooks.js
@@ -2,73 +2,79 @@ import { useState, useEffect } from 'react';
 import { db } from '../../firebase';
 
 export const useGames = (uid) => {
-  const [loading, setLoading] = useState(true);
-  const [games, setGames] = useState([]);
-  
-  useEffect(() => {
-    const unsubscribe = db.games(uid)
-      .onSnapshot(snapshot => {
-        const newGames = snapshot.docs.map(game => ({
-          id: game.id,
-          ...game.data(),
-        }));
-        setGames(newGames);
-        setLoading(false);
-      })
-    return () => unsubscribe();
-  }, [uid])
-  return { games, loading };
-}
+	const [ loading, setLoading ] = useState(true);
+	const [ games, setGames ] = useState([]);
+
+	useEffect(
+		() => {
+			const unsubscribe = db.games(uid).onSnapshot((snapshot) => {
+				const newGames = snapshot.docs.map((game) => ({
+					id: game.id,
+					...game.data()
+				}));
+				setGames(newGames);
+				setLoading(false);
+			});
+			return () => unsubscribe();
+		},
+		[ uid ]
+	);
+	return { games, loading };
+};
 
 export const useGame = (id) => {
-  const [loading, setLoading] = useState(true);
-  const [game, setGame] = useState([]);
-  
-  useEffect(() => {
-    const unsubscribe = db.game(id)
-      .onSnapshot(doc => {
-        setGame(doc.data());
-        setLoading(false);
-      })
-    return () => unsubscribe();
-  }, [id])
-  
-  return { game, loading };
-}
+	const [ loading, setLoading ] = useState(true);
+	const [ game, setGame ] = useState([]);
 
+	useEffect(
+		() => {
+			const unsubscribe = db.game(id).onSnapshot((doc) => {
+				setGame(doc.data());
+				setLoading(false);
+			});
+			return () => unsubscribe();
+		},
+		[ id ]
+	);
+
+	return { game, loading };
+};
 
 export const usePlayers = (gamePlayers) => {
-  const [players, setPlayers] = useState([]);
-  useEffect(() => {
-    setPlayers([]);
-    gamePlayers.forEach(player => {
-      db.user(player)
-        .get()
-        .then(doc => {
-          let pData = doc.data();
-          setPlayers(p => [...p, pData]);
-        })
-    });
-  }, [gamePlayers]);
-  return { players }
-}
+	const [ players, setPlayers ] = useState([]);
+	useEffect(
+		() => {
+			setPlayers([]);
+			gamePlayers.forEach((player) => {
+				db.user(player).get().then((doc) => {
+					let pData = doc.data();
+					setPlayers((p) => [ ...p, pData ]);
+				});
+			});
+		},
+		[ gamePlayers ]
+	);
+	return { players };
+};
 
 export const usePlayerScores = (gameId, playerId) => {
-  const [scores, setScores] = useState([]);
-  const [scoreTotal, setScoreTotal] = useState(0);
-  useEffect(() => {
-    const unsubscribe = db.gamePlayerScores(gameId, playerId)
-      .onSnapshot(doc => {
-        if ( doc.data() ) {
-          setScores(doc.data().scores);
-          let newScoreTotal = 0;
-          doc.data().scores.forEach(score => {
-            newScoreTotal = newScoreTotal + score;
-          })
-          setScoreTotal(newScoreTotal);
-        }
-      })
-      return () => unsubscribe();
-  }, [gameId, playerId])
-  return {scores, scoreTotal}
-}
+	const [ scores, setScores ] = useState([]);
+	const [ scoreTotal, setScoreTotal ] = useState(0);
+	useEffect(
+		() => {
+			const unsubscribe = db.gamePlayerScores(gameId, playerId).onSnapshot((doc) => {
+				if (doc.data()) {
+					setScores(doc.data().scores);
+					let newScoreTotal = 0;
+					doc.data().scores.forEach((score) => {
+						newScoreTotal = newScoreTotal + score;
+					});
+					setScoreTotal(newScoreTotal);
+				}
+			});
+			return () => unsubscribe();
+		},
+		[ gameId, playerId ]
+	);
+	return { scores, scoreTotal };
+};
--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,91 +1,103 @@
 import moment from 'moment';
 import { db, firestore } from './firebase';
-import { FRIEND_STATUS } from '../constants'
+import { FRIEND_STATUS } from '../constants';
 
 // User API
 // ----------------------------------
-export const user = (uid) =>
-  db.collection("users").doc(uid)
+export const user = (uid) => db.collection('users').doc(uid);
 
-export const userByEmail = (email) =>
-  db.collection('users').where("email", "==", email).limit(1)
+export const userByEmail = (email) => db.collection('users').where('email', '==', email).limit(1);
 
 export const createUser = (displayName, email, photoURL, uid) =>
-  db.collection('users').doc(uid).set({ displayName, email, photoURL, uid, created_at: moment().format() })
+	db.collection('users').doc(uid).set({ displayName, email, photoURL, uid, created_at: moment().format('X') });
 
 // Alerts API
 // ----------------------------------
-export const userAlerts = (uid) =>
-  db.collection('users').doc(uid).collection('alerts')
+export const userAlerts = (uid) => db.collection('users').doc(uid).collection('alerts');
 
-export const userAlert = (uid, id) =>
-  user(uid).collection('alerts').doc(id)
+export const userAlert = (uid, id) => user(uid).collection('alerts').doc(id);
 
 export const createUserAlert = (uid, alert) =>
-  user(uid).collection('alerts').add({ ...alert, created_at: moment().format() })
+	user(uid).collection('alerts').add({ ...alert, created_at: moment().format('X') });
 
-export const deleteUserAlert = (uid, alertId) =>
-  user(uid).collection('alerts').doc(alertId).delete()
+export const deleteUserAlert = (uid, alertId) => user(uid).collection('alerts').doc(alertId).delete();
 
 // Friendships API
 // ----------------------------------
-export const friendships = ()  =>
-  db.collection('friendships')
+export const friendships = () => db.collection('friendships');
 
-export const friendship = (id)  =>
-  db.collection('friendships').doc(id)
+export const friendship = (id) => db.collection('friendships').doc(id);
 
 export const createFriendship = async (user, friend) => {
-  let friendshipOne = await db.collection('friendships').add({ uid: user.uid, friend_id: friend.uid, created_at: moment().format()})
-  let friendshipTwo = await db.collection('friendships').add({ uid: friend.uid, friend_id: user.uid, created_at: moment().format()})
-  let inviteAlert = await createUserAlert(friend.uid, {type: "invite", message: `${user.displayName} wants to be friends! Click here to accept.`, link: "FRIENDS" });
-  friendshipOne = await friendshipOne.id;
-  friendshipTwo = await friendshipTwo.id;
-  inviteAlert = await inviteAlert.id;
-  let batch = db.batch();
-  await batch.update(friendship(friendshipOne), { inverse_id: friendshipTwo, status: FRIEND_STATUS.REQUESTER } );
-  await batch.update(friendship(friendshipTwo), { inverse_id: friendshipOne, status: FRIEND_STATUS.REQUESTEE, alertRef: inviteAlert } );
-  const commit = await batch.commit();
-  return commit;
-}
+	let friendshipOne = await db
+		.collection('friendships')
+		.add({ uid: user.uid, friend_id: friend.uid, created_at: moment().format('X') });
+	let friendshipTwo = await db
+		.collection('friendships')
+		.add({ uid: friend.uid, friend_id: user.uid, created_at: moment().format('X') });
+	let inviteAlert = await createUserAlert(friend.uid, {
+		type: 'invite',
+		message: `${user.displayName} wants to be friends! Click here to accept.`,
+		link: 'FRIENDS'
+	});
+	friendshipOne = await friendshipOne.id;
+	friendshipTwo = await friendshipTwo.id;
+	inviteAlert = await inviteAlert.id;
+	let batch = db.batch();
+	await batch.update(friendship(friendshipOne), { inverse_id: friendshipTwo, status: FRIEND_STATUS.REQUESTER });
+	await batch.update(friendship(friendshipTwo), {
+		inverse_id: friendshipOne,
+		status: FRIEND_STATUS.REQUESTEE,
+		alertRef: inviteAlert
+	});
+	const commit = await batch.commit();
+	return commit;
+};
 
 export const acceptFriendRequest = (user, friend) => {
-  let batch = db.batch();
-  batch.update(friendship(friend.id), { status: FRIEND_STATUS.ACCEPTED } );
-  batch.update(friendship(friend.inverse_id), { status: FRIEND_STATUS.ACCEPTED } );
-  batch.delete(userAlert(friend.uid, friend.alertRef));
-  createUserAlert(friend.friend_id, {type: "invite_accepted", message: `${user.displayName} accepted your friend request!`, link: "/friends" });
-  return batch.commit();
-}
+	let batch = db.batch();
+	batch.update(friendship(friend.id), { status: FRIEND_STATUS.ACCEPTED });
+	batch.update(friendship(friend.inverse_id), { status: FRIEND_STATUS.ACCEPTED });
+	batch.delete(userAlert(friend.uid, friend.alertRef));
+	createUserAlert(friend.friend_id, {
+		type: 'invite_accepted',
+		message: `${user.displayName} accepted your friend request!`,
+		link: '/friends'
+	});
+	return batch.commit();
+};
 
 export const removeFriend = (friend) => {
-  let batch = db.batch();
-  batch.delete(friendship(friend.id));
-  batch.delete(friendship(friend.inverse_id));
-  return batch.commit();
-}
+	let batch = db.batch();
+	batch.delete(friendship(friend.id));
+	batch.delete(friendship(friend.inverse_id));
+	return batch.commit();
+};
 
 // Games API
 // ----------------------------------
 
 export const games = (uid) =>
-  db.collection("games").where('players', "array-contains", uid)
+	db.collection('games').where('players', 'array-contains', uid).orderBy('created_at', 'desc');
 
-export const game = (id) =>
-  db.collection("games").doc(id)
+export const game = (id) => db.collection('games').doc(id);
 
 export const createGame = (uid, name, players) =>
-  db.collection("games").add({ uid: uid, name: name, players: players, created_at: moment().format() })
+	db.collection('games').add({ uid: uid, name: name, players: players, created_at: moment().format('X') });
 
-export const gameScores = (gameId) =>
-  game(gameId).collection("scores")
+export const gameScores = (gameId) => game(gameId).collection('scores');
 
-export const gamePlayerScores = (gameId, playerId) =>
-  gameScores(gameId).doc(playerId)
+export const gamePlayerScores = (gameId, playerId) => gameScores(gameId).doc(playerId);
 
 export const addGameScore = (gameId, playerId, score) => {
-  return game(gameId).collection("scores").doc(playerId).set({
-    scores: firestore.FieldValue.arrayUnion(score)
-  }, { merge: true })
-}
-  
+	return game(gameId).collection('scores').doc(playerId).get().then((doc) => {
+		let scores = doc.data().scores;
+		scores.push(score);
+		return game(gameId).collection('scores').doc(playerId).set(
+			{
+				scores: scores
+			},
+			{ merge: true }
+		);
+	});
+};
